911eed2257e8dc3738437d0a2cdeaf0e25985267
HiJobsHoper <115883693+HiJobsHoper@users.noreply.github.com>
2022-10-20 00:21:09 +0800

    Add files via upload

---
+++ Registrasi.java
@@ -0,0 +1,67 @@
+package id.kharisma.studio.hijobs;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Registrasi extends AppCompatActivity {
+
+    private Button btnSimpan;
+    private EditText etNama, etEmail, etTelvon, etPass, etPassKon;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrasi);
+
+        btnSimpan = findViewById(R.id.btnReg_Simpan);
+        etNama = findViewById(R.id.txtReg_Nama);
+        etEmail = findViewById(R.id.txtReg_Email);
+        etTelvon = findViewById(R.id.txtReg_Telepon);
+        etPass = findViewById(R.id.txtReg_KataSandi);
+        etPassKon = findViewById(R.id.txtReg_KonfirmasiSandi);
+
+        btnSimpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Nama = etNama.getText().toString();
+                String Email = etEmail.getText().toString();
+                String Telvon = etTelvon.getText().toString();
+                String Pass = etPass.getText().toString();
+                String PassKon = etPassKon.getText().toString();
+
+                if (Nama.equals("") || Email.equals("") || Telvon.equals("") || Pass.equals("") || PassKon.equals("")) {
+                    Toast.makeText(Registrasi.this,
+                            "Data anda belum lengkap", Toast.LENGTH_LONG).show();
+                } else {
+                    if (Pass.equals(PassKon)) {
+                        //simpan data pada Database
+                    } else {
+                        Toast.makeText(Registrasi.this,
+                                "Kata sandi anda tidak sama", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    /*@Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }*/
+}
