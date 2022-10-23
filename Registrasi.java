58e5d5448c243145c031d963830231d8a99f9425
HiJobsHoper <hijobsbyhoper@gmail.com>
2022-10-23 15:14:54 +0800

    Registrasi

--- Registrasi.java
+++ Registrasi.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +21,7 @@ public class Registrasi extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrasi);
 
+        //Inisialisasi material desain
         btnSimpan = findViewById(R.id.btnReg_Simpan);
         etNama = findViewById(R.id.txtReg_Nama);
         etEmail = findViewById(R.id.txtReg_Email);
@@ -33,27 +35,32 @@ public class Registrasi extends AppCompatActivity {
             public void onClick(View v) {
                 if (cek_Reg() == true) {
                     //halaman selanjutnya ...
-                    //finish();
+                    //finish(); //Menutup halaman registrasi
                 }
             }
         });
 
-        //Membuat tombol back pada Navigasi Bar
+        //Membuat tombol kembali pada Navigasi Bar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 
+    //Kembali ke halaman login
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
+        startActivity(new Intent(Registrasi.this, Login.class)); //Membuka halaman login
+        finish(); //Menutup halaman registrasi
         return true;
     }
 
+    //Mengaktifkan tombol kembali
     @Override
     public void onBackPressed() {
         super.onBackPressed();
     }
 
+    //Memastikan pengisian data sesuai ketentuan
     public boolean cek_Reg() {
         //Inisialisasi data ke dalam variabel
         String Nama = etNama.getText().toString();
@@ -63,7 +70,7 @@ public class Registrasi extends AppCompatActivity {
         String PassKon = etPassKon.getText().toString();
         boolean nilai = false;
 
-        //Memberikan tanda pada data yang belum di isi
+        //Memberikan tanda dan mengarahkan pada data yang belum di isi
         if (PassKon.isEmpty()) {
             etPassKon.setError("Confirm password required");
             etPassKon.requestFocus();
@@ -88,19 +95,20 @@ public class Registrasi extends AppCompatActivity {
         //Mengecek apakah ada data yang belum di isi
         if (Nama.equals("") || Email.equals("") || Telvon.equals("") || Pass.equals("") ||
                 PassKon.equals("")) {
+            //Peringatan data tidak lengkap
             Toast.makeText(Registrasi.this,
                     "Data anda belum lengkap", Toast.LENGTH_LONG).show();
         } else {
             //Memastikan kata sandi dan konfirmasi kata sandi sama
             if (Pass.equals(PassKon)) {
-                nilai = true;
-                //simpan data pada Database
+                //simpan data pada Database ...
+                nilai = true; //Pengisian sesuai ketentuan
             } else {
+                //Peringatan kata sandi dan konfirmasi kata sandi tidak sama
                 Toast.makeText(Registrasi.this,
                         "Kata sandi anda tidak sama", Toast.LENGTH_LONG).show();
             }
         }
-
         return nilai;
     }
 }
